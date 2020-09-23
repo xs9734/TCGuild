8bee33e5e37aa396477f437d6694ef2eb7f8f36f
David <42820350+xs9734@users.noreply.github.com>
2020-09-22 18:32:02 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,31 +1,29 @@
 var neferaJSON = `https://api.tibiadata.com/v2/world/Nefera.json`
 var guildJSON = `https://api.tibiadata.com/v2/guild/Ten+Commandments.json`
 
-
-
-
-
-
 var onlineCounter = document.getElementById("online-counter");
 var onlineCounterGuild = document.getElementById("online-counter-guild");
+
+//All Players Online
     fetch(neferaJSON).then(function (response) {
         return response.json();
     }).then(function (data) {
         //successful response
-        console.log(data.world.world_information.players_online)
+        //console.log(data.world.world_information.players_online)
         onlineCounter.innerHTML = data.world.world_information.players_online;
     }).catch(function (err) {
         //error
         console.warn('Something went wrong.', err);
     });
 
-    fetch(guildJSON).then(function (response) {
+fetch(guildJSON).then(function (response) {
         return response.json();
     }).then(function (data) {
         //successful response
-        console.log(data.guild.data.online_statuss)
         onlineCounterGuild.innerHTML = data.guild.data.online_status;
     }).catch(function (err) {
         //error
         console.warn('Something went wrong.', err);
     });
+
+
