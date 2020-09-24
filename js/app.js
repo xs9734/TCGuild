2e19271ea107c28c7593fa81c5df0b4969266c70
David <42820350+xs9734@users.noreply.github.com>
2020-09-24 05:56:56 -0700

    Online Table Added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var guildJSON = `https://api.tibiadata.com/v2/guild/Ten+Commandments.json`
 
 var onlineCounter = document.getElementById("online-counter");
 var onlineCounterGuild = document.getElementById("online-counter-guild");
+var onlineTable = document.getElementById("online-table");
 
 //All Players Online
     fetch(neferaJSON).then(function (response) {
@@ -16,7 +17,7 @@ var onlineCounterGuild = document.getElementById("online-counter-guild");
         console.warn('Something went wrong.', err);
     });
 
-fetch(guildJSON).then(function (response) {
+    fetch(guildJSON).then(function (response) {
         return response.json();
     }).then(function (data) {
         //successful response
@@ -26,4 +27,45 @@ fetch(guildJSON).then(function (response) {
         console.warn('Something went wrong.', err);
     });
 
+function fetchOnline(){
+    fetch(guildJSON).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        allOnline = [];
+        charData = [];
+        for (var i=0; i<data.guild.members.length;i++){ //loop ranks
+            allOnline.push(data.guild.members[i]);
+            for(var m=0; m<allOnline[i].characters.length;m++){ //loop characters
+                charData.push(data.guild.members[i].characters[m]);
+                if(data.guild.members[i].characters[m].status == "online"){  //add to list if online
+                    var charName = data.guild.members[i].characters[m].name;
+                    var charVocation = data.guild.members[i].characters[m].vocation;
+                    var charLevel = data.guild.members[i].characters[m].level;
+                    var charShareHigh = Math.floor(charLevel * 1.50); //do share range math
+                    var charShareLow =  Math.floor(charLevel * .666); //+-1 lvl accuracy ;(
+                    var entry = document.createElement('tr');
+                    entry.className = 'onlineEntry';
+                    entry.innerHTML =
+                    `
+                        <th scope="row">
+                            <a href="https://www.tibia.com/community/?subtopic=characters&name=${charName}" alt="${charName} tibia page" target="_blank">
+                                ${charName}
+                            </a>
+                        </th>
+                        <td>${charVocation}</td>
+                        <td>${charLevel}</td>
+                        <td>${charShareLow} - ${charShareHigh}</td>
+                    `;
+                    onlineTable.appendChild(entry);
+                }
+                else{                                          //skip offline
 
+                }
+            }
+        }
+
+    }).catch(function (err) {
+        //error
+        console.warn('Something went wrong.', err);
+    });
+}
