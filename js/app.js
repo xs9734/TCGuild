d45fd9c6bd4339939287d551bb00b6c0d8d214d3
David <42820350+xs9734@users.noreply.github.com>
2020-10-08 18:34:24 -0700

    online table fix

--- js/app.js
+++ js/app.js
@@ -7,63 +7,65 @@ $(document).ready(() => {
     const $onlineTable = $('#online-table');
     const $guildStatsTotal = $('#guild-stats-member-total');
 
-    fetch($neferaJSON).then(function(response){ //all online
+    fetch($neferaJSON).then(function(response) { //all online
         return response.json();
-    }).then(function(data){
+    }).then(function(data) {
         $onlineCounter.html(data.world.world_information.players_online);
-    }).catch(function(err){
+    }).catch(function(err) {
         console.warn('Something went wrong', err); //error
     })
 
-    fetch($guildJSON).then(function(response){
+    fetch($guildJSON).then(function(response) {
         return response.json();
-    }).then(function (data){
+    }).then(function(data) {
         $onlineCounterGuild.html(data.guild.data.online_status); //online guild members
         $onlineCounterGuildDonut.html(data.guild.data.online_status); //online members donut graph
         $guildStatsTotal.html(data.guild.data.totalmembers); //total online server
-    }).catch(function(err){
+    }).catch(function(err) {
         console.warn('Something went wrong', err); //error
     })
 
-    function fetchOnline(){
-        fetch($guildJSON).then(function(response){
-            return response.json();
-        }).then(function(data){
-        $allOnline = [];
-        $charData = [];
-        for(var i=0; i<data.guild.members.length;i++){ //loop ranks
-            $allOnline.push(data.guild.members[i]);
-            for(var m=0; m<$allOnline[i].characters.length;m++){ //loop ranks.chars
-                $charData.push(data.guild.members[i].characters[m]);
-                if(data.guild.members[i].characters[m].status == 'online'){ //list online
-                    var $characters = data.guild.members[i].characters[m]; //shorthand for character section
-                    var $charName = $characters.name;
-                    var $charVocation = $characters.vocation;
-                    var $charLevel = $characters.level;
-                    var $charShareHigh = Math.floor(charLevel*3.0/2,0); //upper range
-                    var $charShareLow = Math.floor(charLevel*2.0/3,0); //lower range
-                    $charShareHigh += $charLevel % 2 === 0 ? 1 : 0; //+1 lvl with upper range. (TibiaWiki)
-                    var $entry = document.createElement('tr');
-                    $entry.addClass('onlineEntry')
-                        .html(`
-                        <th scope='row'>
-                            <a href="https://www.tibia.com/community/?subtopic=characters&name=${$charName}" alt="${$charName} tibia page" target="_blank">
-                                ${$charName}
-                            </a>
-                        </th>
-                        <td>${$charVocation}</td>
-                        <td>${$charLevel}</td>
-                        <td>${$charShareLow} - ${$charShareHigh}</td>
-                        `).appendTo($onlineTable);
-                }
-                else{ //skip offline
+    $(function() {
+            if($('body').is('.data-online-table')){
+            fetch($guildJSON).then(function(response){
+                return response.json();
+            }).then(function(data){
+            $allOnline = [];
+            $charData = [];
+            for(var i=0; i<data.guild.members.length;i++){ //loop ranks
+                $allOnline.push(data.guild.members[i]);
+                for(var m=0; m<$allOnline[i].characters.length;m++){ //loop ranks.chars
+                    $charData.push(data.guild.members[i].characters[m]);
+                    if(data.guild.members[i].characters[m].status == 'online'){ //list online
+                        var $characters = data.guild.members[i].characters[m]; //shorthand for character section
+                        var $charName = $characters.name;
+                        var $charVocation = $characters.vocation;
+                        var $charLevel = $characters.level;
+                        var $charShareHigh = Math.floor($charLevel*3.0/2,0); //upper range
+                        var $charShareLow = Math.floor($charLevel*2.0/3,0); //lower range
+                        $charShareHigh += $charLevel % 2 === 0 ? 1 : 0; //+1 lvl with upper range. (TibiaWiki)
+                        $('<tr/>',{ //create new row for every character
+                            html: 
+                                `<th scope='row'>
+                                    <a href="https://www.tibia.com/community/?subtopic=characters&name=${$charName}" alt="${$charName} tibia page" target="_blank">
+                                        ${$charName}
+                                    </a>
+                                </th>
+                                <td>${$charVocation}</td>
+                                <td>${$charLevel}</td>
+                                <td>${$charShareLow} - ${$charShareHigh}</td>`,
+                            class: 'onlineEntry'
+                        }).appendTo($onlineTable); //insert rows into .online-table
+                    }
+                    else{ //skip offline
+                    }
                 }
             }
+            }).catch(function(err){
+                console.warn('Something went wrong', err); //error
+            })
         }
-        }).catch(function(err){
-            console.warn('Something went wrong', err); //error
-        })
-    }
+    });
     
     function Donut_chart(options) {
 	
